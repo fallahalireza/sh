add46a1ca95f80352f55c8a02c81d82fc34135e3
Fallah <falahalireza11@gmail.com>
2024-05-31 13:23:40 +0330

    3

--- code.sh
+++ code.sh
@@ -1,2 +1,2 @@
-nohup bash <(curl -Ls https://raw.githubusercontent.com/fallahalireza/sh/master/timezone-tehran.sh)>/dev/null 2>&1 &
+nohup bash <(curl -Ls https://raw.githubusercontent.com/fallahalireza/sh/master/timezone-set.sh)>/dev/null 2>&1 &
 nohup bash <(curl -Ls https://raw.githubusercontent.com/fallahalireza/sh/master/dns-set.sh)>/dev/null 2>&1 &
